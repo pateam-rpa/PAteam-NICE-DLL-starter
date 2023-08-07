dff1f62831d8001dec635f167ecdc99acafcf1af
mateuszPATeam <60876661+mateuszPATeam@users.noreply.github.com>
2023-08-07 12:29:57 +0200

    Initial commit

---
+++ Template/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Direct.Shared;
+using log4net;
+
+namespace Template
+{
+    [DirectSealed]
+    [DirectDom("YourDLLClassName")]
+    [ParameterType(false)]
+    public class YourDLLClassName
+    {
+        private static readonly ILog _log = LogManager.GetLogger("LibraryObjects");
+
+        [DirectDom("Name of the function in designer")]
+        [DirectDomMethod("This is how this funcion will appear when you select it {param1} {param2}")]
+        [MethodDescription("description")]
+        public static bool TestFunction(int param1, string param2)
+        {
+            return true;
+        }
+    }
+}
---
+++ Template/NewType.cs
@@ -0,0 +1,32 @@
+﻿using Direct.Shared;
+
+namespace Template
+{
+
+    [DirectDom("NewType", "General")]
+    public class NewType : DirectComponentBase
+    {
+        protected PropertyHolder<string> _Name = new PropertyHolder<string>("Name");
+
+
+        [DirectDom("Name")]
+        [DesignTimeInfo("Name")]
+        public string Name
+        {
+            get { return _Name.TypedValue; }
+            set { _Name.TypedValue = value; }
+        }
+
+        public NewType()
+        {
+
+        }
+
+        public NewType(IProject project) : base(project)
+        {
+
+        }
+
+    }
+
+}
